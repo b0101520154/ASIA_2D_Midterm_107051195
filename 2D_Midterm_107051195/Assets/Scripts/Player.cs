06ea67f66108f31e44f861e7444bdda1ea8c4614
b0101520154 <71687764+b0101520154@users.noreply.github.com>
2020-11-12 03:58:50 +0800

    星星道具

--- 2D_Midterm_107051195/Assets/Scripts/Player.cs
+++ 2D_Midterm_107051195/Assets/Scripts/Player.cs
@@ -4,20 +4,21 @@ using UnityEngine.UI;
 public class Player : MonoBehaviour
 {
     public GameObject final;
+    public Text textCount;
     public int count;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.name =="傳送門")
+        if(collision.name == "傳送門")
         {
             final.SetActive(true);
         }
-        if(collision.tag == "寶物")
+        if (collision.tag == "星星")
         {
-            Destroy(collision.gameObject);
-            count++;
-            textCount.text = count + "/3";
+           Destroy(collision.gameObject);
+           count++;
+           textCount.text = count + "/3";
+
         }
     }
-
 }

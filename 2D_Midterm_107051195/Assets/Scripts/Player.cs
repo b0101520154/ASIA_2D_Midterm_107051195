f2b81fd905781a1f775739016faed24015d4fd47
b0101520154 <71687764+b0101520154@users.noreply.github.com>
2020-11-12 01:22:16 +0800

    傳送門完成

---
+++ 2D_Midterm_107051195/Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    public GameObject final;
+    public int count;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.name =="傳送門")
+        {
+            final.SetActive(true);
+        }
+        if(collision.tag == "寶物")
+        {
+            Destroy(collision.gameObject);
+            count++;
+            textCount.text = count + "/3";
+        }
+    }
+
+}
